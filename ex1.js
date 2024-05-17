c8761595a7481ae26f052b3885149af13e69cc22
Andrei Ana-Maria <ana.maria.andrei@ibm.com>
2024-05-17 23:00:49 +0300

    added other methods

--- ex1.js
+++ ex1.js
@@ -5,19 +5,32 @@
 // Output: { titlu: “Pizza”, portii: 3, ingrediente: [”sunca”, “cas”, “ketchup”] }
 
 const createRecipe = (title, servings, ingredients) => {
-    const recipe = {
-        title: title,
-        servings: servings,
-        ingredients: ingredients,
-        displayRecipe: function () {
-            str = `Recipe name is ${this.title},\n You can make ${this.servings} servings with the following ingredients: \n ${this.ingredients}`;
-            return str;
-        }
-    };
-    return recipe;
+  const recipe = {
+    title: title,
+    servings: servings,
+    ingredients: ingredients,
+    displayRecipe: function () {
+      return `Recipe name is ${this.title}, you can make ${this.servings} servings with the following ingredients ${this.ingredients}`;
+    },
+    toString: function() {
+      return `{ title: ${this.title}, servings: ${this.servings}, ingredients: ${this.ingredients} }`;
+    }
+  };
+  return recipe;
 }
 
 const recipeOne = createRecipe('Pizza', '3', ['sunca', 'cas', 'sos de rosii']);
 const recipeTwo = createRecipe('Tiramisu', 6, ['mascarpone', 'cafea', 'piscoturi']);
 console.log(recipeOne.displayRecipe());
 console.log(recipeTwo.displayRecipe());
+
+// met 2
+
+console.log(recipeOne.toString());
+console.log(recipeTwo.toString());
+
+
+// met 3
+
+console.log(JSON.stringify(recipeOne));
+console.log(JSON.stringify(recipeTwo));
